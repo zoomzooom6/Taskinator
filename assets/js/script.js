53e9fc38dc2bcb34793a6b0d701a1726101ace89
zoomzoom6 <tonyhuang107@live.ca>
2021-05-16 18:29:43 -0400

    read input from user and add text to list

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,23 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do");
 var createTaskHandler = function() {
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //create list item
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+    //give it a class name
+    taskInfoE1.className = "task-info";
+    //add HTML content to div
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    listItemE1.append(taskInfoE1);
+
+    //add entire list item to list
     tasksToDoE1.appendChild(listItemE1);
 }
 
