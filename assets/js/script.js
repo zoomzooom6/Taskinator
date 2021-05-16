4bf547ad67db6f3685dd93a85f31f6a823c7053a
zoomzoom6 <tonyhuang107@live.ca>
2021-05-16 18:10:48 -0400

    restablish functionality of adding tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+    event.preventDefault();
+
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task.";
     tasksToDoE1.appendChild(listItemE1);
 }
 
-buttonE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
