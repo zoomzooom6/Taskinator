a21def715fc8a61b09c0b9b7734d6cbf14c66e25
zoomzoom6 <tonyhuang107@live.ca>
2021-05-16 18:58:28 -0400

    check for empty inputs and reset form after entry

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,14 @@ var taskFormHandler = function () {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formE1.reset();
+    
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
